0b8c0a6f0a4771cd08ceafcc6f5d2af584e5ffe7
lloker735 <magerkovlad337@gmail.com>
2026-05-13 10:36:51 +0600

    6 практика с доделанным 5 заданием

--- Задание_5/sample/index.php
+++ Задание_5/sample/index.php
@@ -8,55 +8,22 @@
     <h1>Функции</h1>
     <h2>Встроенные функции, часть 2</h2>
     <?php
-        require "teams.php";
-        require "albums.php";
-        require "tracks.php";
-        
-        if (isset($_GET['search']) && $_GET['search'] != '') {
-            $parts = explode('::', $_GET['search']);
-            
-            if (count($parts) == 2) {
-                $entity = $parts[0];  
-                $id = $parts[1]; 
-                
-                if ($entity == 'teams') {
-                    $data = $content;  
-                    $title = "Группа";
-                } elseif ($entity == 'albums') {
-                    $data = $content;  
-                    $title = "Альбом";
-                } elseif ($entity == 'tracks') {
-                    $data = $content;  
-                    $title = "Трек";
-                } else {
-                    echo "<p>Ошибка: неизвестная сущность '$entity'</p>";
-                    $data = [];
-                }
-                
-                $found = false;
-                foreach ($data as $item) {
-                    if ($item['id'] == $id) {
-                        echo "<h3>$title (id=$id)</h3>";
-                        echo "<pre>";
-                        print_r($item);
-                        echo "</pre>";
-                        $found = true;
-                        break;
-                    }
-                }
-                
-                if (!$found) {
-                    echo "<p>Запись с id=$id не найдена в $entity</p>";
-                }
-                
-            } else {
-                echo "<p>Ошибка: неверный формат. Используйте entity::id</p>";
+        require "dump/personnel.php";
+        $id = isset($_GET['id']) && $_GET['id'] !== '' ? (int)$_GET['id'] : 1;
+        $found = false;
+        foreach ($personnel as $person) {
+            if ($person['id_personnel'] == $id) {
+                echo "<h3>Преподаватель (id = $id)</h3>";
+                echo "<pre>";
+                print_r($person);
+                echo "</pre>";
+                $found = true;
+                break;
             }
-            
-        } else {
-            echo "<p>?search=teams::3</p>";
+        }
+        if (!$found) {
+            echo "<p>Преподаватель с id = $id не найден</p>";
         }
     ?>
-    
 </body>
 </html>
